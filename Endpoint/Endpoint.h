7ccfe1aab982b646ef930dc9b71cf32634a8bf2d
Suyeol Jeon <devxoul@gmail.com>
2016-01-07 03:07:06 +0900

    Start Xcode project

---
+++ Endpoint/Endpoint.h
@@ -0,0 +1,19 @@
+//
+//  Endpoint.h
+//  Endpoint
+//
+//  Created by 전수열 on 1/7/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Endpoint.
+FOUNDATION_EXPORT double EndpointVersionNumber;
+
+//! Project version string for Endpoint.
+FOUNDATION_EXPORT const unsigned char EndpointVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Endpoint/PublicHeader.h>
+
+

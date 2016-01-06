0cf27020f6afd993d9c726df4e0b06233fa63ef6
Suyeol Jeon <devxoul@gmail.com>
2016-01-07 03:43:09 +0900

    Add simple test cases

--- EndpointTests/EndpointTests.swift
+++ EndpointTests/EndpointTests.swift
@@ -7,30 +7,56 @@
 //
 
 import XCTest
-@testable import Endpoint
+
+import Alamofire
+import Endpoint
+
+enum GitHub: Rule, Endpoint {
+
+    static var baseURLString = "https://api.github.com"
+
+    case User = "GET /user/{username}"
+
+    case Repo = "GET /repos/{owner}/{repo}"
+    case RepoIssues  = "GET  /repos/{owner}/{repo}/issues"
+    case CreateIssue = "POST /repos/{owner}/{repo}/issues"
+
+}
 
 class EndpointTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testBuildURL() {
+        {
+            let values = ["owner": "devxoul", "repo": "Then"]
+            let (method, URL, otherValues) = GitHub.Repo.buildURL(values)
+            XCTAssertEqual(method, Alamofire.Method.GET)
+            XCTAssertEqual(URL.absoluteString, "https://api.github.com/repos/devxoul/Then")
+            XCTAssertTrue(otherValues?.isEmpty == true)
+        }();
+        {
+            let values = ["owner": "devxoul", "repo": "Then"]
+            let (method, URL, otherValues) = GitHub.RepoIssues.buildURL(values)
+            XCTAssertEqual(method, Alamofire.Method.GET)
+            XCTAssertEqual(URL.absoluteString, "https://api.github.com/repos/devxoul/Then/issues")
+            XCTAssertTrue(otherValues?.isEmpty == true)
+        }();
+        {
+            let values = ["owner": "devxoul", "repo": "Then", "title": "Hello, World!"]
+            let (method, URL, otherValues) = GitHub.CreateIssue.buildURL(values)
+            XCTAssertEqual(method, Alamofire.Method.POST)
+            XCTAssertEqual(URL.absoluteString, "https://api.github.com/repos/devxoul/Then/issues")
+            XCTAssertEqual(otherValues?.count, 1)
+            XCTAssertEqual(otherValues!["title"] as? String, "Hello, World!")
+        }();
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testRequest() {
+        {
+            let values = ["owner": "devxoul", "repo": "Then", "title": "Hello, World!"]
+            let request = GitHub.CreateIssue.request(values)
+            XCTAssertEqual(request.request?.HTTPMethod, "POST")
+            XCTAssertEqual(request.request?.URL?.absoluteString, "https://api.github.com/repos/devxoul/Then/issues")
+        }();
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
